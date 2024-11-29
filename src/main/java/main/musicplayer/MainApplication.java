ed51eaefbbf57cf579168f2fe6aff0358a963f2f
Neeraj Ciju <danciju@gmail.com>
2024-11-29 16:39:45 +0530

    Made stage transparent

--- src/main/java/main/musicplayer/MainApplication.java
+++ src/main/java/main/musicplayer/MainApplication.java
@@ -3,7 +3,9 @@ package main.musicplayer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,7 +14,10 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MusicPlayerGui.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
         stage.setTitle("Music Player");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
