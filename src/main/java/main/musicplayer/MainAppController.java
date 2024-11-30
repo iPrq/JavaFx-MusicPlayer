64a1420f0681ec256fee37c92dfbdb47440b3a0e
Neeraj Ciju <danciju@gmail.com>
2024-11-30 10:58:13 +0530

    Added functionality to play and pause buttons

--- src/main/java/main/musicplayer/MainAppController.java
+++ src/main/java/main/musicplayer/MainAppController.java
@@ -1,6 +1,7 @@
 package main.musicplayer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
@@ -11,7 +12,11 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
-public class MainAppController {
+import java.io.File;
+import java.net.URL;
+import java.util.*;
+
+public class MainAppController implements Initializable {
 
     @FXML
     private ImageView close;
@@ -28,6 +33,13 @@ public class MainAppController {
     @FXML
     private ProgressBar songProgressBar;
     private double x,y;
+    private File directory;
+    private File[] files;
+    private ArrayList<File> songs;
+    private int songNumber= 0;
+    private Timer timer;
+    private TimerTask timerTask;
+    private boolean running;
     Media media;
     MediaPlayer mediaPlayer;
 
@@ -36,6 +48,20 @@ public class MainAppController {
 
     @FXML
     private Pane titlePane;
+    @Override
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        songs = new ArrayList<File>();
+        directory = new File("src/main/resources/music");
+        files = directory.listFiles();
+        if (files != null) {
+            songs.addAll(Arrays.asList(files));
+        }
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        songNamelbl.setText(songs.get(songNumber).getName());
+    }
+
     public void init(Stage stage) {
         titlePane.setOnMousePressed(MouseEvent -> {
                 x = MouseEvent.getSceneX();
@@ -51,11 +77,28 @@ public class MainAppController {
 
     @FXML
     void playBtn(MouseEvent event) {
-
+        if(mediaPlayer.getStatus()==MediaPlayer.Status.PLAYING) {
+            mediaPlayer.pause();
+        }
+        else {
+            mediaPlayer.play();
+        }
     }
 
     @FXML
     void prevBtn(MouseEvent event) {
+    }
+
+    @FXML
+    void nextBtn(MouseEvent event) {
+
+    }
+
+    void beginTimer() {
+
+    }
+
+    void cancelTimer() {
 
     }
 
